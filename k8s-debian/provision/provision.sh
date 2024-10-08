92fec7afdf27d688bd67ee6a9e8a972644da97b7
Jed Chaves <86432601+jedchaves@users.noreply.github.com>
2024-10-08 20:22:26 -0300

    Update provision.sh

--- k8s-debian/provision/provision.sh
+++ k8s-debian/provision/provision.sh
@@ -10,10 +10,10 @@ sed 's,\r$,,' /vagrant/files/id_ed25519.pub > /root/.ssh/authorized_keys
 
 HOSTS=$(head -n7 /etc/hosts)
 echo -e "$HOSTS" > /etc/hosts
-echo '192.168.56.100 control.k8s.local' >> /etc/hosts
-echo '192.168.56.111 node1.k8s.local' >> /etc/hosts
-echo '192.168.56.112 node2.k8s.local' >> /etc/hosts
-echo '192.168.56.50 storage.k8s.local' >> /etc/hosts
+echo '192.168.56.10 control.k8s.local' >> /etc/hosts
+echo '192.168.56.11 node1.k8s.local' >> /etc/hosts
+echo '192.168.56.12 node2.k8s.local' >> /etc/hosts
+echo '192.168.56.30 storage.k8s.local' >> /etc/hosts
 
 if [ "$HOSTNAME" != "storage" ]; then
   sed -Ei 's/(.*swap.*)/#\1/g' /etc/fstab
