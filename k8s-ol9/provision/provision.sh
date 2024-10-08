fccdaed98033410fe7fa34f32942e10a7f5b493b
Jed Chaves <86432601+jedchaves@users.noreply.github.com>
2024-10-08 20:21:09 -0300

    Update provision.sh

--- k8s-ol9/provision/provision.sh
+++ k8s-ol9/provision/provision.sh
@@ -7,10 +7,10 @@ cp /vagrant/files/id_ed25519.pub /root/.ssh/authorized_keys
 
 HOSTS=$(head -n7 /etc/hosts)
 echo -e "$HOSTS" > /etc/hosts
-echo '192.168.56.100 control.k8s.local' >> /etc/hosts
-echo '192.168.56.111 worker1.k8s.local' >> /etc/hosts
-echo '192.168.56.112 worker2.k8s.local' >> /etc/hosts
-echo '192.168.56.50 storage.k8s.local' >> /etc/hosts
+echo '192.168.56.10 control.k8s.local' >> /etc/hosts
+echo '192.168.56.11 worker1.k8s.local' >> /etc/hosts
+echo '192.168.56.12 worker2.k8s.local' >> /etc/hosts
+echo '192.168.56.30 storage.k8s.local' >> /etc/hosts
 
 if [ "$HOSTNAME" != "storage" ]; then
   sed -Ei 's/(.*swap.*)/#\1/g' /etc/fstab
